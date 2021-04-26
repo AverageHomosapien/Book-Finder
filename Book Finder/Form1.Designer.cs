8fbce37684214d9212aec16e353f499a80723c16
Calum <calum.hamilton@outlook.com>
2021-04-26 14:42:28 +0100

    Initial commit

---
+++ Book Finder/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Book_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.APIbutton = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.infoBox = new System.Windows.Forms.TextBox();
+            this.simpleAPI = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // APIbutton
+            // 
+            this.APIbutton.AccessibleName = "API";
+            this.APIbutton.Location = new System.Drawing.Point(243, 77);
+            this.APIbutton.Name = "APIbutton";
+            this.APIbutton.Size = new System.Drawing.Size(75, 23);
+            this.APIbutton.TabIndex = 0;
+            this.APIbutton.Text = "API";
+            this.APIbutton.UseVisualStyleBackColor = true;
+            this.APIbutton.Click += new System.EventHandler(this.APIbutton_click);
+            // 
+            // output
+            // 
+            this.output.AccessibleName = "output";
+            this.output.Location = new System.Drawing.Point(95, 238);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(365, 20);
+            this.output.TabIndex = 1;
+            // 
+            // input
+            // 
+            this.input.AccessibleName = "output";
+            this.input.Location = new System.Drawing.Point(95, 117);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(365, 20);
+            this.input.TabIndex = 2;
+            // 
+            // infoBox
+            // 
+            this.infoBox.AccessibleName = "output";
+            this.infoBox.Location = new System.Drawing.Point(95, 175);
+            this.infoBox.Name = "infoBox";
+            this.infoBox.Size = new System.Drawing.Size(365, 20);
+            this.infoBox.TabIndex = 3;
+            // 
+            // simpleAPI
+            // 
+            this.simpleAPI.AccessibleName = "API";
+            this.simpleAPI.Location = new System.Drawing.Point(243, 32);
+            this.simpleAPI.Name = "simpleAPI";
+            this.simpleAPI.Size = new System.Drawing.Size(75, 23);
+            this.simpleAPI.TabIndex = 4;
+            this.simpleAPI.Text = "API";
+            this.simpleAPI.UseVisualStyleBackColor = true;
+            this.simpleAPI.Click += new System.EventHandler(this.simpleAPI_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 483);
+            this.Controls.Add(this.simpleAPI);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.APIbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button APIbutton;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox infoBox;
+        private System.Windows.Forms.Button simpleAPI;
+    }
+}
+
---
+++ Book Finder/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Book_Finder
+{
+    public partial class Form1 : Form
+    {
+        // https://developers.google.com/books/docs/v1/getting_started#REST
+        public const string myLib = "https://www.googleapis.com/books/v1/mylibrary/";
+        public const string URL = "https://www.googleapis.com/books/v1/volumes/";
+        public const string URLPnP = "https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAK";
+        // Search for quilting - https://www.googleapis.com/books/v1/volumes?q=quilting
+        // Search Harry Potter https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse
+        //Use a comma-separated list to select multiple fields.
+
+
+        HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            output.Text = "";
+        }
+
+        private void APIbutton_click(object sender, EventArgs e)
+        {
+            String urlParameters = "?" + input.Text; 
+
+            // Add an Accept header for JSON format
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+
+            //infoBox.Text = response.Content.ReadAsStringAsync().ToString();
+
+            infoBox.Text = client.GetAsync(urlParameters).Result.Content.ToString();
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result; // Add a reference to System.Net.Http.Formatting.dll
+                output.Text = "Success: ";
+                foreach (var d in dataObjects)
+                {
+                    output.Text += d.Name + "\n";
+                }
+            }
+            else
+            {
+                output.Text = "Fail: " + (int)response.StatusCode + " " + response.ReasonPhrase;
+            }
+
+            // Other HTTP Client calls here
+
+            
+
+            //client.Dispose(); // Once calls are complete
+        }
+
+        private void simpleAPI_Click(object sender, EventArgs e)
+        {
+            client.BaseAddress = new Uri(URLPnP);
+            String urlParameters = "?";
+
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            infoBox.Text = response.Content.ReadAsStringAsync().Result; // Fills infobox with all the content
+            JObject joResponse = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            
+            
+            // a lil bit of meta data; VolumeInfo - info on book; sale info - how much and site;
+            // .....access info - country and viewability (PDF and EPUB); -- all ojObject parse
+            JObject ojObject = (JObject)joResponse["volumeInfo"];
+            //JArray array = (JArray)ojObject["title"];
+
+            //JObject ojObject = (JObject)joResponse["volumeInfo"];
+            //JArray array = (JArray)ojObject["title"];
+
+            //JArray array = (JObject)joResponse["volumeInfo"];
+            //string title = ojObject[0].ToString();
+
+            // output.Text = ojObject.ToString(); -- prints volume info
+            output.Text = array.ToString();
+            
+            if (response.IsSuccessStatusCode)
+            {
+                
+
+                //JsonConvert.DeserializeObject< response.Content.ReadAsAsync<IEnumerable<DataObject>>() > (string json);
+                //output.Text = infoBox.Text
+
+
+
+                /*
+                foreach (var d in response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result)
+                {
+                    System.Console.WriteLine(d.Name);
+                }
+
+                /*
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;
+                output.Text = "Success: \n";
+
+                foreach (var d in dataObjects)
+                {
+                    output.Text += d.Name + "\n";
+                }
+                */
+            }
+            else
+            {
+                output.Text = "Fail: " + (int)response.StatusCode + " " + response.ReasonPhrase;
+            }
+        }
+
+        public class DataObject
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+        [Serializable]
+        public class BookObject
+        {
+            //public string 
+        }
+    }
+}
